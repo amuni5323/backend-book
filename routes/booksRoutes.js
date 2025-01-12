0f79a5f64189ca5c560dc5a3f5b94afc4971b5b8
amuni5323 <amuni11.hamai@gmail.com>
2025-01-12 07:12:52 +0300

    kkkkf

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -36,9 +36,9 @@ router.post('/', authenticate, async (req, res) => {
 // GET all books for logged-in user
 router.get('/', authenticate, async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
-    const limit = parseInt(req.query.limit) || 10; // Default to limit 10 if not provided
-    const skip = (page - 1) * limit;
+    // const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    // const limit = parseInt(req.query.limit) || 10; // Default to limit 10 if not provided
+    // const skip = (page - 1) * limit;
 
 
 
